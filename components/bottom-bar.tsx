6beba05e9c56d281daa43ad80b36ad7433035768
Escartem <escartem.yt@gmail.com>
2024-09-11 14:41:44 +0000

    changement lien github

--- components/bottom-bar.tsx
+++ components/bottom-bar.tsx
@@ -20,7 +20,7 @@ export default function BottomBar() {
     return (
         <div className="h-[30px] bg-slate-900 w-full flex items-center justify-evenly">
             <Button onClick={() => {updateGroup()}} className="text-white" variant="link">{`Changer de groupe (${group?.split("@")[1]})`}</Button>
-            <Button onClick={() => {window.open("https://github.com/Escartem/EDTVelizy", "_blank")}} className="text-white" variant="link">GitHub</Button>
+            <Button onClick={() => {window.open("https://github.com/iut-velizy/EDTVelizy", "_blank")}} className="text-white" variant="link">GitHub</Button>
             <Button disabled={true} className="text-white" variant="link">Activer les notifications</Button>
         </div>
     )
