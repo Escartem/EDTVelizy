1e83a3a259cd1e01d7c53642e272ea21d8a57ce2
Escartem <escartem.yt@gmail.com>
2025-03-24 10:20:48 +0000

    ts

--- lib/secureLogin.ts
+++ lib/secureLogin.ts
@@ -2,9 +2,12 @@ import crypto from "crypto";
 
 const key = process.env.MASTER_KEY;
 
+// @ts-ignore
 export function encrypt(text: string) {
     const iv = crypto.randomBytes(12);
+    // @ts-ignore
     const cipher = crypto.createCipheriv("aes-256-gcm", Buffer.from(key, "hex"), iv);
+    // @ts-ignore
     const encrypted = Buffer.concat([cipher.update(text, "utf8"), cipher.final()]);
     const authTag = cipher.getAuthTag();
     return `${iv.toString("hex")}:${encrypted.toString("hex")}:${authTag.toString("hex")}`;
